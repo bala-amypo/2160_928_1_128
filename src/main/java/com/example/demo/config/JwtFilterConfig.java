681d96e3a8e77ed42adc769a3d2d589905677066
bala-amypo <bala@amypo.in>
2025-12-19 08:10:41 +0000

    User commit

--- src/main/java/com/example/demo/config/JwtFilterConfig.java
+++ src/main/java/com/example/demo/config/JwtFilterConfig.java
@@ -1,1 +1,25 @@
-asdf
+package com.example.demo.config;
+
+import com.example.demo.security.JwtAuthFilter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+public class JwtFilterConfig {
+
+    private final JwtAuthFilter jwtAuthFilter;
+
+    public JwtFilterConfig(JwtAuthFilter jwtAuthFilter) {
+        this.jwtAuthFilter = jwtAuthFilter;
+    }
+
+    @Bean
+    public JwtAuthFilter jwtAuthFilter() {
+        return jwtAuthFilter;
+    }
+
+    public Class<? extends UsernamePasswordAuthenticationFilter> beforeFilter() {
+        return UsernamePasswordAuthenticationFilter.class;
+    }
+}
