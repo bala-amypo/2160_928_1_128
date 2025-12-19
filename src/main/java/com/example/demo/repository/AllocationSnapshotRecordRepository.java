520b23ce204db57b2c48d800f3b69b3861db3033
bala-amypo <bala@amypo.in>
2025-12-19 13:07:20 +0000

    User commit

--- src/main/java/com/example/demo/repository/AllocationSnapshotRecordRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repository;
-
-import java.util.List;
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.demo.entity.AllocationSnapshotRecord;
-
-public interface AllocationSnapshotRecordRepository extends JpaRepository<AllocationSnapshotRecord, Long> {
-    List<AllocationSnapshotRecord> findByInvestorId(Long investorId);
-}
