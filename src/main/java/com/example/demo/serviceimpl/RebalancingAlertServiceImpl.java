97e1ed81cfa503e1eb030dfa11876b2ce44d37b0
bala-amypo <bala@amypo.in>
2025-12-20 06:56:37 +0000

    User commit

---
+++ src/main/java/com/example/demo/serviceimpl/RebalancingAlertServiceImpl.java
@@ -0,0 +1,49 @@
+package com.example.demo.serviceimpl;
+
+import com.example.demo.entity.RebalancingAlertRecord;
+import com.example.demo.repository.RebalancingAlertRecordRepository;
+import com.example.demo.service.RebalancingAlertService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class RebalancingAlertServiceImpl implements RebalancingAlertService {
+
+    private final RebalancingAlertRecordRepository repository;
+
+    public RebalancingAlertServiceImpl(RebalancingAlertRecordRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public RebalancingAlertRecord createAlert(RebalancingAlertRecord alert) {
+        if (alert.getCurrentPercentage() <= alert.getTargetPercentage()) {
+            throw new RuntimeException("Invalid Alert");
+        }
+        return repository.save(alert);
+    }
+
+    @Override
+    public RebalancingAlertRecord resolveAlert(Long id) {
+        RebalancingAlertRecord alert = getAlertById(id);
+        alert.setResolved(true);
+        return repository.save(alert);
+    }
+
+    @Override
+    public RebalancingAlertRecord getAlertById(Long id) {
+        return repository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Entity not found"));
+    }
+
+    @Override
+    public List<RebalancingAlertRecord> getAlertsByInvestor(Long investorId) {
+        return repository.findByInvestorId(investorId);
+    }
+
+    @Override
+    public List<RebalancingAlertRecord> getAllAlerts() {
+        return repository.findAll();
+    }
+}
